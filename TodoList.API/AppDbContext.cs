de50740977ec1b98cf3aa7cda96c2fb7a4cfa2ca
A00256013 <a00256013@mycambrian.ca>
2024-02-13 23:54:00 -0500

    Added AppDbContext.cs file and set the property for ToDoItem entity and constructor for AppDbContext

---
+++ TodoList.API/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TodoList.API
+{
+    public class AppDbContext
+    {
+        // DbSet property for ToDoItem entity
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+        // Constructor for AppDbContext
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
